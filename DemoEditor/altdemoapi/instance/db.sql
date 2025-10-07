e7ff8433e8c1a07f76fac348494b4ec107b19c76
Efren Haskell <efrenhask@gmail.com>
2025-10-07 16:40:50 -0400

    Added table schema to instance

--- DemoEditor/altdemoapi/instance/db.sql
+++ DemoEditor/altdemoapi/instance/db.sql
@@ -1,1 +1,6 @@
-
+CREATE OR REPLACE TABLE Student(
+    id              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name      VARCHAR(15),
+    last_name       VARCHAR(15),
+    email_address   VARCHAR(50) NOT NULL
+);
