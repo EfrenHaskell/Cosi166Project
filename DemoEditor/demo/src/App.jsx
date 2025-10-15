e1c57d97ea2350ab747901fd501a869dcc510c2d
joseph-sosa <josephsosa@brandeis.edu>
2025-10-14 23:20:49 -0400

    added Login component with css styling

--- DemoEditor/demo/src/App.jsx
+++ DemoEditor/demo/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import Login from "./components/Login";
 import TeacherMode from "./components/TeacherMode";
 import StudentMode from "./components/StudentMode";
 import MainNotes from "./components/MainNotes";
@@ -7,27 +8,73 @@ import Sidebar from "./components/Sidebar";
 import DarkLightTheme from "./components/DarkLightTheme";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userType, setUserType] = useState(null);
   const [teacherMode, setTeacherMode] = useState(false);
+
+  const handleLogin = (type, username, password) => {
+    // You can add your hardcoded credentials validation here
+    // For now, we'll just accept any non-empty credentials
+    console.log(`Login attempt: ${type} - ${username}`);
+    
+    setIsLoggedIn(true);
+    setUserType(type);
+    
+    if (type === "teacher") {
+      setTeacherMode(true);
+    } else {
+      setTeacherMode(false);
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUserType(null);
+    setTeacherMode(false);
+  };
 
   return (
     <>
-      {/* make basic  login feature that goes to teacher mode or student mode based on sign information provided*/}
+      {!isLoggedIn ? (
+        <Login onLogin={handleLogin} />
+      ) : (
+        <>
+          {/* use to toggle lightmode on and off*/}
+          <DarkLightTheme />
+          
+          {/* Logout button */}
+          <div style={{ textAlign: 'right', padding: '10px' }}>
+            <button 
+              onClick={handleLogout}
+              style={{
+                padding: '8px 16px',
+                backgroundColor: '#dc3545',
+                color: 'white',
+                border: 'none',
+                borderRadius: '4px',
+                cursor: 'pointer'
+              }}
+            >
+              Logout ({userType})
+            </button>
+          </div>
 
-      {/* use to toggle lightmode on and off*/}
-      {<DarkLightTheme/>}
+          {userType === "teacher" ? (
+            <TeacherMode teacherMode={teacherMode} setTeacherMode={setTeacherMode} />
+          ) : (
+            <StudentMode />
+          )}
 
-      <TeacherMode teacherMode={teacherMode} setTeacherMode={setTeacherMode} />
+          <hr />
 
-      {!teacherMode && <StudentMode />}
+          {/* the jsx below is for the notes app please do not touch!!!!!! */}
 
-      <hr />
-
-      {/* the jsx below is for the notes app please do not touch!!!!!! */}
-
-      {/* <div className="notes_app">
-        <Sidebar />
-        <MainNotes />
-      </div> */}
+          {/* <div className="notes_app">
+            <Sidebar />
+            <MainNotes />
+          </div> */}
+        </>
+      )}
     </>
   );
 }
