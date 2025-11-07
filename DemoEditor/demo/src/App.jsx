102bb9f410be0f2ecd317b0ac232a3e85df684fe
Ppandey12 <paraspandey827@gmail.com>
2025-11-07 15:39:36 -0500

    hade a hover effect on notes and added a router so code code question opens in new tab

--- DemoEditor/demo/src/App.jsx
+++ DemoEditor/demo/src/App.jsx
@@ -4,24 +4,34 @@ import TeacherMode from "./components/TeacherMode";
 import StudentMode from "./components/StudentMode";
 import MainNotes from "./components/MainNotes";
 import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [teacherMode, setTeacherMode] = useState(false);
 
   return (
     <>
-      <TeacherMode teacherMode={teacherMode} setTeacherMode={setTeacherMode} />
+      {/* <TeacherMode teacherMode={teacherMode} setTeacherMode={setTeacherMode} />
 
       {!teacherMode && <StudentMode />}
 
-      <hr />
+      <hr /> */}
 
       {/* the jsx below is for the notes app please do not touch!!!!!! */}
-
-      {/* <div className="notes_app">
-        <Sidebar />
-        <MainNotes />
-      </div> */}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="notes_app">
+                <Sidebar />
+                <MainNotes />
+              </div>
+            }
+          />
+          <Route path="/code-question" element={<StudentMode />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- DemoEditor/demo/src/components/Sidebar.jsx
+++ DemoEditor/demo/src/components/Sidebar.jsx
@@ -1,25 +1,33 @@
 import React from "react";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+  const openCodeQuestion = () => {
+    window.open("/code-question", "_blank"); // opens in new tab
+  };
   return (
     <div className="sideBar">
       <div className="sidebar_header">
         <h1>Class Notes</h1>
-        <button>Add Note</button>
       </div>
 
-      <div className="notes">
-        <div className="note">
-          <div className="note_title">
-            <strong>Title</strong>
-            <button>Delete Note</button>
-          </div>
-
-          <p>Note Preview</p>
-          <small className="note_meta">
-            last modofied [date]
-          </small>
+      <div className="note">
+        <div
+          className="note_title"
+          onClick={() => setOpen(!open)}
+          style={{ cursor: "pointer" }}
+        >
+          <p className="title">Class 1</p>
         </div>
+
+        {open && (
+          <div className="note_content">
+            <button onClick={openCodeQuestion}>Code Question</button>
+            <button>Text Question</button>
+            <button>Question</button>
+          </div>
+        )}
       </div>
     </div>
   );
